bacc8be28b2c950aca36080aa07b0e5e35087d84
MinhTuan083 <yuunohop3@gmail.com>
2024-06-03 00:43:17 +0700

    truong hop wrong

--- projectvuejs/src/App.vue
+++ projectvuejs/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <main-screen v-if="statusMatch === 'default'" @onStart="onHandleBeforeStart($event)"/>
-  <interact-screen v-if="statusMatch === 'match'" />
+  <interact-screen v-if="statusMatch === 'match'" :cardsContext="setting.cardsContext" />
 </template>
 
 <script>
 import MainScreen from "./components/MainScreen.vue";
 import InteractScreen from "./components/InteractScreen.vue";
+import { shuffled } from "./utils/array";
+import {} from "./utils/array";
 export default {
   name: 'App',
   data(){
     return{
+      setting: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
       statusMatch: "default", 
     };
   },
@@ -20,6 +27,21 @@ export default {
     methods:{
       onHandleBeforeStart(config) {
         console.log("running handle before start, ", config);
+        this.setting.totalOfBlocks = config.totalOfBlocks;
+
+        const firstCards = Array.from({ length: this.setting.totalOfBlocks / 2 }, 
+        (_, i) => i + 1
+      );
+       const secondCards = [...firstCards];
+       const cards = [...firstCards, ...secondCards];
+       
+       this.setting.cardsContext = shuffled(shuffled(shuffled(shuffled(cards))));
+
+       this.setting.startedAt = new Date().getTime();
+
+
+
+        //data ready
         this.statusMatch = "match";
       },
     },
--- projectvuejs/src/components/InteractScreen.vue
+++ projectvuejs/src/components/InteractScreen.vue
@@ -1,3 +1,64 @@
 <template>
     <h1>Interact Componet here...</h1>
+    <card-flip v-for="(card, index) in cardsContext" 
+    :key="index" 
+    :ref="`card-${index}`"
+    :imageBackFaceUrl="`images/${card}.png`" 
+    :card="{ index, value: card}"
+    @onFlip="checkRule($event)"
+    />
+
 </template>
+
+<script>
+import CardFlip from "./CardFruit.vue";
+export default{
+    props: {
+        cardsContext:{
+            type: Array,
+            default: function () {
+                return [];
+                
+            },
+        },
+    },
+    components: {
+        CardFlip,
+    },
+    data(){
+        return{
+            rules: [],
+        };
+    },
+    methods: {
+        checkRule(card){
+                if(this.rules.length === 2 ) return false;
+
+                this.rules.push(card);
+            
+
+                if(this.rules.length === 2 && this.rules[0].value === this.rules[1].value){
+                    console.log("Right...");
+                    
+                }
+                else if( this.rules.length === 2 &&
+                 this.rules[0].value !== this.rules[1].value){
+                    
+                    console.log("Wrong...");
+
+                   setTimeout(() => {
+                     //close two card
+                     this.$refs[`card-${this.rules[0].index}`][0].onFlipBackCard()
+                    this.$refs[`card-${this.rules[1].index}`][0].onFlipBackCard()
+
+                    //reset rules
+                    this.rules = [];
+                   }, 800);
+
+                    
+                 } else return false;
+        }
+    }
+};
+
+</script>
