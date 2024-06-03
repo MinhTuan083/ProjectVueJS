76f7d07685792f3bab315e18f5eb94134c09b0b3
phi1235 <phipari12345@gmail.com>
2024-06-04 04:04:12 +0700

    commit new game caro

--- projectvuejs/src/App.vue
+++ projectvuejs/src/App.vue
@@ -1,31 +1,60 @@
-<template>
-  <main-screen v-if="statusMatch === 'default'" @onStart="onHandleBeforeStart($event)"/>
-  <interact-screen v-if="statusMatch === 'match'" />
+
+    
+    <template>
+  <div id="app">
+    <div v-if="showOtherGames" class="other-games">
+      <main-screen
+        v-if="statusMatch === 'default'"
+        @onStart="onHandleBeforeStart($event)"
+      />
+      <interact-screen v-if="statusMatch === 'match'" />
+      <router-view
+        v-if="statusMatch !== 'default' && statusMatch !== 'match'"
+      />
+
+      <img src="/path/to/other-game-info.png" alt="Other Games Info" />
+    </div>
+    <router-view @toggleOtherGames="toggleOtherGames" />
+  </div>
 </template>
+  
+
 
 <script>
 import MainScreen from "./components/MainScreen.vue";
 import InteractScreen from "./components/InteractScreen.vue";
+
 export default {
-  name: 'App',
-  data(){
-    return{
-      statusMatch: "default", 
+  name: "App",
+  data() {
+    return {
+      statusMatch: "default",
     };
   },
   components: {
     MainScreen,
     InteractScreen,
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      console.log("running handle before start, ", config);
+      this.statusMatch = "match";
     },
-    methods:{
-      onHandleBeforeStart(config) {
-        console.log("running handle before start, ", config);
-        this.statusMatch = "match";
-      },
-    },
+  },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
 </style>
---
+++ projectvuejs/src/components/HomeBoard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="home-container">
+    <h1>Cờ Caro</h1>
+    <button @click="startGameWithBot" @mouseover="hover = true" @mouseleave="hover = false" class="hover-button">
+      Đánh với máy
+      <img v-if="hover" src="@/assets/image/ronaldo.png" alt="Hover Image" class="hover-image">
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hover: false
+    };
+  },
+  methods: {
+    startGameWithBot() {
+      this.$router.push({ name: 'GameBoard' });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.home-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: url('@/assets/image/bgr.jpg'); /* Path to your background image */
+  background-size: cover;
+  background-position: center;
+  
+}
+
+.hover-button {
+  position: relative;
+  padding: 10px 20px;
+  font-size: 20px;
+  cursor: pointer;
+  border: none;
+  background-color: #007bff;
+  color: white;
+  border-radius: 5px;
+  outline: none;
+}
+h1 {
+  font-size: 3em;
+  color: #020202;
+}
+.hover-image {
+  position: absolute;
+  top: -300px; /* Adjust based on your layout */
+  left: 50%;
+  transform: translateX(-50%);
+  width: 300px; /* Adjust based on your image size */
+  height: auto;
+  display: none;
+}
+
+.hover-button:hover .hover-image {
+  display: block;
+}
+</style>
+
+
+
+
+
+
